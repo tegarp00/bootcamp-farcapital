bfdd5144f5c447aad72a3551c4b88969c0737380
tegarp00 <tegarp00@gmail.com>
2022-11-16 11:58:54 +0700

    add responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {
     container: {
@@ -23,5 +26,7 @@ module.exports = {
 		}
 	},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
